56a6887c75bc610595524c802b309430513a25cb
abdullahmansss <abdullah.mansour97@gmail.com>
2020-07-21 20:10:12 +0200

    mansour

--- app/src/main/java/com/example/mvvm3/MainActivity.java
+++ app/src/main/java/com/example/mvvm3/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        // hello
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
